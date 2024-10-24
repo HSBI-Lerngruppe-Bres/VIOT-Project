5213c1961566e0191ff076ea3cc6e66f2935c7aa
Nils Baxheinrich <nils@nbax.de>
2024-10-24 09:39:01 +0200

    Refactor server.py to include MQTT client subscription and graceful shutdown

--- ControlServer/server.py
+++ ControlServer/server.py
@@ -1,19 +1,22 @@
 from config import Config
 from database_connector import DatabaseConnector
-from mqtt_client import MQTTClient
 from email_server import EmailServer
 import paho.mqtt.client as mqtt
 
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 
-def on_connect(client, userdata, flags, rc):
+def send_emails(email_server, subject, body, email_addresses):
+    pass
+
+def on_connect(mqtt_client, userdata, flags, rc):
     """
     Callback function for when the client receives a CONNACK response from the server.
 
     Args:
-        client (mqtt.Client): The client instance for this callback.
+        mqtt_client (mqtt.Client): The client instance for this callback.
         userdata (any): The private user data as set in Client() or userdata_set().
         flags (dict): Response flags sent by the broker.
         rc (int): The connection result.
@@ -23,54 +26,76 @@ def on_connect(client, userdata, flags, rc):
     else:
         logger.error(f"Connect failed with code {rc}")
 
-def on_disconnect(client, userdata, rc):
+def on_disconnect(mqtt_client, userdata, rc):
     """
     Callback function for when the client disconnects from the broker.
 
     Args:
-        client (mqtt.Client): The client instance for this callback.
+        mqtt_client (mqtt.Client): The client instance for this callback.
         userdata (any): The private user data as set in Client() or userdata_set().
         rc (int): The disconnection result.
     """
     logger.info("Disconnected with result code " + str(rc))
 
-def on_message(client, userdata, msg):
+def on_message(mqtt_client, userdata, msg):
     """
     Callback function for when a PUBLISH message is received from the server.
 
     Args:
-        client (mqtt.Client): The client instance for this callback.
+        mqtt_client (mqtt.Client): The client instance for this callback.
         userdata (any): The private user data as set in Client() or userdata_set().
         msg (mqtt.MQTTMessage): The message instance containing topic and payload.
     """
     logger.info(f"Received message: {msg.payload} on topic: {msg.topic}")
+    
+    splitted_topic = msg.topic.split('/')
+    event_type = splitted_topic[2]
+    sensor_id = splitted_topic[1]
+    db_connector = userdata['db_connector']
+    email_server = userdata['email_server']    
+
+    if event_type == "weight":
+        weight = json.loads(msg.payload)["value"]
+        db_connector.add_weight(sensor_id=sensor_id, weight=weight)
+        upper_threshold = db_connector.get_upper_threshold(sensor_id)
+        average_weight = db_connector.get_average_weight(sensor_id)
+        if average_weight + upper_threshold > weight:
+            return
+        email_adresses = db_connector.get_email_adresses(sensor_id)
+        package_weight = weight - (average_weight + upper_threshold)
+        send_emails(email_server, "NEW PACKAGE", f"New package detected with weight {package_weight}", email_adresses)
+        lower_threshold = weight - db_connector.get_threshold_sensitivity(sensor_id)
+        mqtt_client.publish(f"mailbox/{sensor_id}/arm_alarm", json.dumps({"value": lower_threshold}))
+    elif event_type == "alarm":
+        alarm = json.loads(msg.payload)["value"]
+        db_connector.add_alarm(sensor_id=sensor_id, alarm=alarm)
+        email_adresses = db_connector.get_email_adresses(sensor_id)
+        send_emails(email_server, "ALARM", f"Alarm detected with value {alarm}", email_adresses)
+    else:
+        logger.warning(f"Unknown event type: {event_type}")
+        
+    
+    value = json.loads(msg.payload)["value"]
 
 def main():
-    # Load configuration
     config = Config('config.yml')
     
-    # Set up logging
     logging.basicConfig(level=config.log_level.upper(),
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
-    # Create a file handler
     file_handler = logging.FileHandler(config.log_file)
     file_handler.setLevel(config.log_level.upper())
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     
-    # Create a console handler
     console_handler = logging.StreamHandler()
     console_handler.setLevel(config.log_level.upper())
     console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     
-    # Get the logger and set the handlers
     logger.addHandler(file_handler)
     logger.addHandler(console_handler)
     
-    # Log configuration details
     logger.info("Configuration loaded successfully.")
     
-    # Initialize database connector
     db_connector = DatabaseConnector(
         config.db_user, 
         config.db_password, 
@@ -80,13 +105,6 @@ def main():
         logger=logger
     )
     
-    mqtt_client = mqtt.Client()
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_disconnect = on_disconnect
-    mqtt_client.on_message = on_message
-    mqtt_client.username_pw_set(config.mqtt_username, config.mqtt_password)
-    mqtt_client.connect(config.mqtt_broker, config.mqtt_port, 60)
-    
     email_server = EmailServer(
         config.email_smtp_server, 
         config.email_port, 
@@ -94,9 +112,19 @@ def main():
         config.email_password
     )
     
-    mqtt_client.subscribe("test_topic")
-    mqtt_client.publish("test_topic", "Hello, World!")
-
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_disconnect = on_disconnect
+    mqtt_client.on_message = on_message
+    mqtt_client.username_pw_set(config.mqtt_username, config.mqtt_password)
+    mqtt_client.connect(config.mqtt_broker, config.mqtt_port, 60)
+    mqtt_client.user_data_set({
+        'db_connector': db_connector,
+        'email_server': email_server
+    })
+    
+    mqtt_client.subscribe("mailbox/+/weight")
+    mqtt_client.subscribe("mailbox/+/alarm")
     try:
         mqtt_client.loop_forever()
     except KeyboardInterrupt:
